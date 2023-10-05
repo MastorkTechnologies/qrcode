ed98a1ef35695406f08cb8e00dda5deb32859dbd
apurbar06 <apurbar06@gmail.com>
2023-10-05 20:38:00 +0530

    sign in with google

--- qr-app/src/components/Navbar/Navbar.jsx
+++ qr-app/src/components/Navbar/Navbar.jsx
@@ -1,19 +1,50 @@
-import style from "./Navbar.module.css"
-import logo from "../../Images/QRCODE.png"
+import { useEffect, useState } from "react";
+import style from "./Navbar.module.css";
+import logo from "../../Images/QRCODE.png";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../../firebase";
+import { useNavigate } from "react-router-dom";
 
-export default function Navbar(){
-    return(
-        <div className={style.block}>
-            <div className={style.logo}>
-                <img src={logo}/>
-            </div>
-            <ul>
-                <li>Features</li>
-                <li>How it Works</li>
-                <li>Use Cases</li>
-                <li>FAQ</li>
-            </ul>
-            <button className={style.startbtn}>Get Started</button>
-        </div>
-    )
+export default function Navbar() {
+  const [signedIn, setSignedIn] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setSignedIn(true);
+      } else {
+        console.log("user is signed out", user);
+      }
+    });
+  }, []);
+
+  // Signout function
+  const logout = () => {
+    auth.signOut();
+    setSignedIn(false);
+  };
+
+  return (
+    <div className={style.block}>
+      <div className={style.logo}>
+        <img src={logo} />
+      </div>
+      <ul>
+        <li>Features</li>
+        <li>How it Works</li>
+        <li>Use Cases</li>
+        <li>FAQ</li>
+      </ul>
+      {signedIn === true ? (
+        <button className={style.startbtn} onClick={logout}>
+          Log Out
+        </button>
+      ) : (
+        <button className={style.startbtn} onClick={() => navigate("/login")}>
+          Log In
+        </button>
+      )}
+    </div>
+  );
 }
--- qr-app/src/pages/Login/Login.jsx
+++ qr-app/src/pages/Login/Login.jsx
@@ -1,20 +1,41 @@
-import style from "./Login.module.css"
-import googleicon from "../../Images/google_g_logo.svg.png"
-import arrow from "../../Images/right-arrow.png"
+import style from "./Login.module.css";
+import googleicon from "../../Images/google_g_logo.svg.png";
+import arrow from "../../Images/right-arrow.png";
+import { auth, provider } from "../../firebase";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
 
-export default function Login(){
-    return(<div className={style.upperblock}>
+export default function Login() {
+  const navigate = useNavigate();
 
-        <div className={style.block}>
-         <h2>Sign in to your account</h2>
-         <button><img className={style.googleicon} src={googleicon} /> Continue with Google</button>
-          <p>or</p>
-          <div  className={style.inputblock}>
+  // Sign in with google
+  const signin = () => {
+    auth.signInWithPopup(provider).catch(alert);
+  };
 
-          <input type="password" name="" id="" placeholder="Email"   />
-           <img className={style.arrow} src={arrow}/>
-          </div>
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        navigate("/");
+      }
+    });
+  }, []);
+
+  return (
+    <div className={style.upperblock}>
+      <div className={style.block}>
+        <h2>Sign in to your account</h2>
+        <button onClick={signin}>
+          <img className={style.googleicon} src={googleicon} /> Continue with
+          Google
+        </button>
+        <p>or</p>
+        <div className={style.inputblock}>
+          <input type="password" name="" id="" placeholder="Email" />
+          <img className={style.arrow} src={arrow} />
         </div>
+      </div>
     </div>
-    )
+  );
 }
